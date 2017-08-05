875ae548ed62a6d48f59b34206c149961601ce31
swamygit <swamydhulipalla@gmail.com>
2017-08-05 23:10:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title: 'Article one | Swamy',
     heading: 'Article one',
     date: 'Aug 5 2017',
