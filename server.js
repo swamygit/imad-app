5769361f27d1403471bec9f499b712aaca548c8f
swamygit <swamydhulipalla@gmail.com>
2017-08-06 06:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,23 +120,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
-
-app.get('/article-two', function (req, res) {
-  res.send("hi this article-two");
-});
-
-app.get('/article-three', function (req, res) {
-  res.send("hi this article-three");
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
